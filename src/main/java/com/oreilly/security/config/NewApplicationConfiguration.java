c18996716b761180daa87e288531da733c5f45fd
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-06-09 20:15:51 +0200

    Initial jdbc config

--- src/main/java/com/oreilly/security/config/NewApplicationConfiguration.java
+++ src/main/java/com/oreilly/security/config/NewApplicationConfiguration.java
@@ -1,10 +1,22 @@
 package com.oreilly.security.config;
 
+import javax.annotation.Priority;
+import javax.sql.DataSource;
+
 import org.h2.server.web.WebServlet;
+import org.hibernate.engine.jdbc.connections.spi.DataSourceBasedMultiTenantConnectionProviderImpl;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.authentication.UserServiceBeanDefinitionParser;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -34,6 +46,21 @@ public class NewApplicationConfiguration implements WebMvcConfigurer{
 	    @Bean
 	    public static NoOpPasswordEncoder passwordEncoder() {
 	    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	    }
+	    @Bean
+	    public DataSource dataSource() {
+	    	return new EmbeddedDatabaseBuilder()
+	    			.setType(EmbeddedDatabaseType.H2)
+	    			.addScript("schema-h2.sql")
+	    			.build();
+	    	 
+	    }
+	    @Bean
+	    @Primary
+	    public UserDetailsService userDetailsServiceJdbc() {
+	    	JdbcDaoImpl jdbcDao = new JdbcDaoImpl();
+	    	jdbcDao.setDataSource(this.dataSource());
+	    	return jdbcDao;
 	    }
 
 	
