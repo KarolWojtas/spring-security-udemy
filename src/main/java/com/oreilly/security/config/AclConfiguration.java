5c5b966b902d8aba74487c2d6082b6773392f860
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-06-19 21:23:05 +0200

    Finished Java Configuration for ACL

---
+++ src/main/java/com/oreilly/security/config/AclConfiguration.java
@@ -0,0 +1,89 @@
+package com.oreilly.security.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.cache.ehcache.EhCacheFactoryBean;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.acls.AclPermissionCacheOptimizer;
+import org.springframework.security.acls.AclPermissionEvaluator;
+import org.springframework.security.acls.domain.AclAuthorizationStrategyImpl;
+import org.springframework.security.acls.domain.ConsoleAuditLogger;
+import org.springframework.security.acls.domain.DefaultPermissionGrantingStrategy;
+import org.springframework.security.acls.domain.EhCacheBasedAclCache;
+import org.springframework.security.acls.jdbc.BasicLookupStrategy;
+import org.springframework.security.acls.jdbc.JdbcMutableAclService;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true)
+public class AclConfiguration extends GlobalMethodSecurityConfiguration{
+
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+
+	}
+	@Bean
+	public SimpleGrantedAuthority adminAuthority() {
+		return new SimpleGrantedAuthority("ROLE_ADMIN");
+	}
+	@Bean
+	public AclAuthorizationStrategyImpl aclAuthStrategy() {
+		return new AclAuthorizationStrategyImpl(adminAuthority());
+	}
+	@Bean
+	public ConsoleAuditLogger auditLogger() {
+		return new ConsoleAuditLogger();
+	}
+	@Bean
+	public EhCacheManagerFactoryBean aclCacheManager() {
+	    return new EhCacheManagerFactoryBean();
+	}
+	@Bean
+	public EhCacheFactoryBean aclEhCacheFactoryBean() {
+	    EhCacheFactoryBean ehCacheFactoryBean = new EhCacheFactoryBean();
+	    ehCacheFactoryBean.setCacheManager(aclCacheManager().getObject());
+	    ehCacheFactoryBean.setCacheName("aclCache");
+	    return ehCacheFactoryBean;
+	}
+	@Bean
+	public EhCacheBasedAclCache aclCache() {
+		return new EhCacheBasedAclCache(
+				aclEhCacheFactoryBean().getObject(), 
+				new DefaultPermissionGrantingStrategy(auditLogger()), 
+				aclAuthStrategy());
+	}
+	@Bean
+	BasicLookupStrategy lookupStrategy() {
+		return new BasicLookupStrategy(dataSource(), aclCache(), aclAuthStrategy(), auditLogger());
+	}
+	@Bean
+	JdbcMutableAclService aclService() {
+		return new JdbcMutableAclService(dataSource(), lookupStrategy(), aclCache());
+	}
+	@Bean
+	AclPermissionEvaluator permissionEvaluator() {
+		return new AclPermissionEvaluator(aclService());
+	}
+	@Bean
+	AclPermissionCacheOptimizer permissionCacheOptimizer() {
+		return new AclPermissionCacheOptimizer(aclService());
+	}
+	@Bean
+	DefaultMethodSecurityExpressionHandler expressionHandler() {
+		DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+		expressionHandler.setPermissionCacheOptimizer(permissionCacheOptimizer());
+		expressionHandler.setPermissionEvaluator(permissionEvaluator());
+		return expressionHandler;
+	}
+
+
+}
--- src/main/java/com/oreilly/security/config/WebSecurityConfig.java
+++ src/main/java/com/oreilly/security/config/WebSecurityConfig.java
@@ -1,10 +1,18 @@
 package com.oreilly.security.config;
 
 import javax.servlet.FilterConfig;
+import javax.sql.DataSource;
 
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.acls.jdbc.JdbcMutableAclService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.BeanIds;
@@ -28,88 +36,78 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.oreilly.security.services.CustomAuthenticationFilter;
 import com.oreilly.security.services.CustomUserDetailsService;
+
 @EnableWebSecurity
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled=true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthenticationProvider provider;
 	@Autowired
 	private UserDetailsService userService;
+
 	public WebSecurityConfig() {
 		// TODO Auto-generated constructor stub
 	}
-	 @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	        http
-	            .authorizeRequests()
-	            
-	            	.antMatchers("/appointments/").hasAnyRole("USER","ADMIN")
-	            	.antMatchers("/schedule/").hasAnyRole("ADMIN")
-	            	.antMatchers("/h2-console").permitAll()
-	            	.antMatchers("/**").hasAnyRole("ANONYMOUS","USER","ADMIN")
-	                .and()
-	                /*.formLogin()
-	                	.loginPage("/login")
-	                	.loginProcessingUrl("/login")
-	                	.usernameParameter("custom_username")
-	                	.passwordParameter("custom_password")
-	                	.defaultSuccessUrl("/appointments/")
-	                	.failureUrl("/login?error=true")*/
-	                //.and()
-	                	.logout()
-	                	.logoutUrl("/logout")
-	                	.logoutSuccessUrl("/login?logout=true")
-	                .and()
-	                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-	                .userDetailsService(userService)
-	                	.csrf()
-	                	.disable()
-	                .headers()
-	                	.frameOptions().disable()
-	                .and()
-	                .exceptionHandling()
-	                	.authenticationEntryPoint(loginEntryPoint())
-	                	.accessDeniedPage("/login")
-	             	;
-	        
-	    }
 
-	  
-	    @Bean
-	    public DefaultWebSecurityExpressionHandler defaultExpressinonhandler() {
-	    	return new DefaultWebSecurityExpressionHandler();
-	    }
-	    // tu podany jest adres gdzie odsyłać gdy user nie ma dostępu
-	    @Bean
-	    public LoginUrlAuthenticationEntryPoint loginEntryPoint() {
-	    	return new LoginUrlAuthenticationEntryPoint("/login");
-	    }
-	    @Bean 
-	    public CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
-	    	SavedRequestAwareAuthenticationSuccessHandler succHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-	    	succHandler.setDefaultTargetUrl("/appointments/");
-	    	//succHandler.setAlwaysUseDefaultTargetUrl(true);
-	    	CustomAuthenticationFilter filter = new CustomAuthenticationFilter();
-	    	
-	    	SimpleUrlAuthenticationFailureHandler failHandler = new SimpleUrlAuthenticationFailureHandler();
-	    	failHandler.setDefaultFailureUrl("/login?error=true");
-	    	filter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/login", "POST"));
-	    	filter.setAuthenticationManager(this.authenticationManager());
-	    	filter.setUsernameParameter("custom_username");
-	    	filter.setPasswordParameter("custom_password");
-	    	filter.setAuthenticationSuccessHandler(succHandler);
-	    	filter.setAuthenticationFailureHandler(failHandler);
-	    	
-	    	return filter;
-	    }
-	    @Override
-	    protected void configure(
-	      AuthenticationManagerBuilder auth) throws Exception {
-	  
-	        auth.authenticationProvider(provider);
-	        
-	    }
-	   
-	    
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+
+				.antMatchers("/appointments/").hasAnyRole("USER", "ADMIN").antMatchers("/schedule/").hasAnyRole("ADMIN")
+				.antMatchers("/h2-console").permitAll().antMatchers("/**").hasAnyRole("ANONYMOUS", "USER", "ADMIN")
+				.and()
+				/*
+				 * .formLogin() .loginPage("/login") .loginProcessingUrl("/login")
+				 * .usernameParameter("custom_username") .passwordParameter("custom_password")
+				 * .defaultSuccessUrl("/appointments/") .failureUrl("/login?error=true")
+				 */
+				// .and()
+				.logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout=true").and()
+				.addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.userDetailsService(userService).csrf().disable().headers().frameOptions().disable().and()
+				.exceptionHandling().authenticationEntryPoint(loginEntryPoint()).accessDeniedPage("/login");
+
+	}
+
+	@Bean
+	public DefaultWebSecurityExpressionHandler defaultExpressinonhandler() {
+		return new DefaultWebSecurityExpressionHandler();
+	}
+
+	// tu podany jest adres gdzie odsyłać gdy user nie ma dostępu
+	@Bean
+	public LoginUrlAuthenticationEntryPoint loginEntryPoint() {
+		return new LoginUrlAuthenticationEntryPoint("/login");
+	}
+
+	@Bean
+	public CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
+		SavedRequestAwareAuthenticationSuccessHandler succHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+		succHandler.setDefaultTargetUrl("/appointments/");
+		// succHandler.setAlwaysUseDefaultTargetUrl(true);
+		CustomAuthenticationFilter filter = new CustomAuthenticationFilter();
+
+		SimpleUrlAuthenticationFailureHandler failHandler = new SimpleUrlAuthenticationFailureHandler();
+		failHandler.setDefaultFailureUrl("/login?error=true");
+		filter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/login", "POST"));
+		filter.setAuthenticationManager(this.authenticationManager());
+		filter.setUsernameParameter("custom_username");
+		filter.setPasswordParameter("custom_password");
+		filter.setAuthenticationSuccessHandler(succHandler);
+		filter.setAuthenticationFailureHandler(failHandler);
+
+		return filter;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.authenticationProvider(provider);
+
+	}
+
+
+	
+
 }
